8e93c767bffef58cfd200d5d273e714d09c06fdc
MotasamAhmad <134956918+MotasamAhmad@users.noreply.github.com>
2023-05-30 17:53:35 +0300

    Add files via upload

---
+++ assets/dist/js/dark-mod.js
@@ -0,0 +1,24 @@
+const inputEl = document.querySelector(".input");
+
+const bodyEl = document.querySelector("body");
+
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+
+updateBody();
+
+function updateBody() {
+  if (inputEl.checked) {
+    bodyEl.style.background = "RGBA(33,37,41,var(--bs-bg-opacity,1))";
+  } else {
+    bodyEl.style.background = "white";
+  }
+}
+
+inputEl.addEventListener("input", () => {
+  updateBody();
+  updateLocalStorage();
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
